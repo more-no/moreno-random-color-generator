f3ea0cef27b022119968aac8bdfc79abe7116925
Moreno Dore <moreno.dore@tuta.io>
2023-09-07 16:50:42 +0200

    Add library for random color

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { argv } from 'node:process';
 import chalk from 'chalk';
-import { randomHexColor } from 'random-hex-color-generator';
+import randomColor from 'randomcolor';
 
-let colorFrame = randomHexColor();
+let colorFrame = randomColor({ hue: argv[2], luminosity: argv[3] });
 
 const hash = '#';
 const space = ' ';
@@ -18,15 +18,4 @@ const createFrame = () => {
   console.log(chalk.hex(colorFrame)(line1 + '\n' + line1 + '\n' + line1));
 };
 
-if (argv.length === 3) {
-  //  colorFrame = ??? (argv[2]);         converter missing
-  createFrame();
-} else if (argv.length < 3) {
-  createFrame();
-} else if (argv.length === 4) {
-  if (argv[3] === 'light') {
-    colorFrame = `${argv[2] + 'Bright'}`;
-  } else if (argv[3] === 'dark') {
-    colorFrame = argv[2];
-  }
-}
+createFrame();
